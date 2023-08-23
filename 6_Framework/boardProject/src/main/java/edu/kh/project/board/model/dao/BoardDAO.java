408af488b8e3c2d53c7dae9d1ee2098a6cb10dda
banseok <rjh658@naver.com>
2023-08-23 18:20:25 +0900

    dsfdsfdsf

--- 6_Framework/boardProject/src/main/java/edu/kh/project/board/model/dao/BoardDAO.java
+++ 6_Framework/boardProject/src/main/java/edu/kh/project/board/model/dao/BoardDAO.java
@@ -59,7 +59,7 @@ public class BoardDAO {
 		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
 		
 		// 3. selectList("namespace.id", 파라미터, rowBounds)
-		return sqlSession.selectList("boardMapper.selectBoardList", boardCode, rowBounds);
+		return sqlSession.selectList("boardMapper.selectBoardList", boardCode, rowBounds); // rowBounds
 		
 		
 		
@@ -84,5 +84,45 @@ public class BoardDAO {
 	 */
 	public int boardLikeCheck(Map<String, Object> map) {
 		return sqlSession.selectOne("boardMapper.boardLikeCheck", map);
+	}
+
+
+	
+	/** 좋아요 테이블 삽입
+	 * @param paramMap
+	 * @return result
+	 */
+	public int insertBoardLike(Map<String, Integer> paramMap) {
+		return sqlSession.insert("boardMapper.insertBoardLike", paramMap);
+	}
+
+
+
+	/** 좋아요 테이블 삭제
+	 * @param paramMap
+	 * @return result
+	 */
+	public int deleteBoardLike(Map<String, Integer> paramMap) {
+		return sqlSession.delete("boardMapper.deleteBoardLike", paramMap);
+	}
+
+
+
+	/** 좋아요 개수 조회
+	 * @param boardNo
+	 * @return count
+	 */
+	public int CountBoardLike(Integer boardNo) {
+		return sqlSession.selectOne("boardMapper.CountBoardLike", boardNo);
+	}
+
+
+	
+	/** 조회수 증가
+	 * @param boardNo
+	 * @return count
+	 */
+	public int updateReadCount(int boardNo) {
+		return sqlSession.update("boardMapper.updateReadCount", boardNo);
 	}
 }
