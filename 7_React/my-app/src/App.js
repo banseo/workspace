cd83c4f5b1492fbc73772c2fe06c90cb9bdfcb27
banseok <rjh658@naver.com>
2023-09-05 10:58:13 +0900

    11

---
+++ 7_React/my-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+// components 폴더의 Exam1.js를 가져와서 사용
+// 사용할 때 이름을 Ex1으로 지정
+import Ex1 from './components/Exam1';
+
+function App() {
+  // 리액트의 컴포넌트는 딱 하나의 요소만을 반환할 수 있다.
+  // -> 여러 요소를 반환하고 싶을 때는 부모요소로 묶어준다!
+  return (
+    /* fragment (<></>) : 반환 되는 요소를 감쌀 때 사용 , 해석 x */
+    <>
+      {/* jsx 주석 */}
+      <h1>Hello React!!?</h1>
+  
+      <div>리액트 배운다~</div>
+
+      <Ex1></Ex1>
+    </>
+  );
+}
+
+export default App;
